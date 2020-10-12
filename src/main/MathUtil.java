2743c994134434210876a244c7c140eb67157050
Charles Bartels <charlieb2898@gmail.com>
2020-10-12 13:01:58 -0600

    added mutiple ways to test math operations

--- src/main/MathUtil.java
+++ src/main/MathUtil.java
@@ -6,4 +6,20 @@ public class MathUtil {
 		return a + b;
 		
 	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int devide(int a, int b) {
+		return a / b;
+	}
+	
+	public int mutiply(int a, int b) {
+		return a * b;
+	}
+	
+	public double testCircumference(double a) {
+		return Math.PI * 2*a;
+	}
 }
--- src/test/TestMath.java
+++ src/test/TestMath.java
@@ -17,9 +17,9 @@ class TestMath {
 	@BeforeEach
 	void intit() {
 //		Create instance
-		System.out.println("Creating");
+		
 		calc = new MathUtil();
-		System.out.println("Created");
+	
 	}
 
 	
@@ -27,13 +27,59 @@ class TestMath {
 	@Test
 	void testAdd() {
 		
-		// Expect 
-				int expected = 2;
-		// Actual 
-				int actual = calc.add(1, 1);
-		// Verify 
-				assertEquals(actual, expected);
+// 		Expect 
+		int expected = 2;
+//		Actual 
+		int actual = calc.add(1, 1);
+// 		Verify 
+		assertEquals(actual, expected);
 			
 	}
+	
+	@DisplayName("Testing Subtraction")
+	@Test
+	void testSubtract() {
+//		Expected
+		int expected = 3;
+// 		Actual
+		int actual = calc.subtract(6, 3);
+// 		Verify
+		assertEquals(actual, expected);
+		
+	}
+	
+	@DisplayName("Testing Devision")
+	@Test 
+	void testDivide() {
+//		Expected 
+		int expected = 3;
+//		Actual 
+		int actual = calc.devide(9, 3);
+//		Verify 
+		assertEquals(actual, expected);
+	}
+	
+	@DisplayName("Testing Multiplication")
+	@Test 
+	void testMutiply() {
+//		Expected 
+		int expected = 6;
+//		Actual 
+		int actual = calc.mutiply(3, 2);
+//		Verify 
+		assertEquals(actual, expected);
+	}
+	
+	@DisplayName("Find Circumfrence")
+	@Test 
+	void testCircumfrence() {
+//		Expected 
+		double expected = 12 * Math.PI;
+//		Actual 
+		double actual = calc.testCircumference(6);
+//		Verify 
+		assertEquals(actual, expected);
+	}
+
 
 }
